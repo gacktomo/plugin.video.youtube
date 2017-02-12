102d14900e923fe18e648e24caa9975d8c5b59b4
Tomohiro Matsuo <gacktomo@gmail.com>
2017-02-13 04:23:17 +0900

    auto play related video

--- resources/lib/youtube/helper/yt_play.py
+++ resources/lib/youtube/helper/yt_play.py
@@ -14,6 +14,7 @@ def play_video(provider, context, re_match):
     try:
         video_id = context.get_param('video_id')
         client = provider.get_client(context)
+
         video_streams = client.get_video_streams(context, video_id)
         if len(video_streams) == 0:
             message = context.localize(provider.LOCAL_MAP['youtube.error.no_video_streams_found'])
@@ -34,9 +35,22 @@ def play_video(provider, context, re_match):
 
         if video_stream.get('meta', None):
             video_item.set_subtitles(video_stream['meta'].get('subtitles', None))
-
         video_id_dict = {video_id: video_item}
         utils.update_video_infos(provider, context, video_id_dict)
+
+        #get related video and set to queue
+        videos = []
+        json_data = client.get_related_videos(video_id)
+        if not v3.handle_error(provider, context, json_data):
+            return False
+        result = v3.response_to_items(provider, context, json_data, process_next_page=False)
+        videos.extend(result)
+        context.log_debug('video: "%s' % videos[0])
+        playlist = context.get_video_playlist()
+        context.log_debug('playlist: "%s' % playlist)
+        # playlist.clear()
+        # playlist.add(video_item)
+        playlist.add(random.choice(videos[0:7]))
 
         # Trigger post play events
         if provider.is_logged_in():
@@ -141,6 +155,7 @@ def play_playlist(provider, context, re_match):
 
     # add videos to playlist
     for video in videos:
+        context.log_debug('video: "%s' % video)
         playlist.add(video)
         pass
 
